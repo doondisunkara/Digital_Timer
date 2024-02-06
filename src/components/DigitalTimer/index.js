4538f5b47fc81909a0e84ead079c78053fefd2c0
doondisunkara <sunkaranag2004@gmail.com>
2024-02-06 16:10:59 +0000

    Incomplete few left

--- src/components/DigitalTimer/index.js
+++ src/components/DigitalTimer/index.js
@@ -3,15 +3,29 @@ import './index.css'
 
 class DigitalTimer extends Component {
   state = {
-    startTimer: false,
+    isTimerRunning: false,
+    timer: 25,
+  }
+
+  onClickPlayPause = () => {
+    this.setState(prev => ({isTimerRunning: !prev.isTimerRunning}))
+  }
+
+  increaseTimer = () => {
+    this.setState(prev => ({timer: prev.timer + 1}))
+  }
+
+  decreaseTimer = () => {
+    this.setState(prev => ({timer: prev.timer - 1}))
   }
 
   render() {
-    const {startTimer} = this.state
-    const playImgUrl = startTimer
+    const {isTimerRunning, timer} = this.state
+    const playImgUrl = isTimerRunning
       ? 'https://assets.ccbp.in/frontend/react-js/pause-icon-img.png'
       : 'https://assets.ccbp.in/frontend/react-js/play-icon-img.png'
-    const playAltVal = startTimer ? 'pause' : 'play'
+    const playAltVal = isTimerRunning ? 'pause' : 'play'
+    const playButton = isTimerRunning ? 'Pause' : 'Start'
     return (
       <div className="app-container">
         <h1>Digital Timer</h1>
@@ -24,14 +38,18 @@ class DigitalTimer extends Component {
           <div className="timer-features-container">
             <div className="controls-container">
               <div className="control">
-                <button type="button" className="button">
+                <button
+                  type="button"
+                  className="button"
+                  onClick={this.onClickPlayPause}
+                >
                   <img
                     src={playImgUrl}
                     alt={playAltVal}
                     className="option-img"
                   />
                 </button>
-                <p>Start</p>
+                <p className="interaction-text">{playButton}</p>
               </div>
               <div className="control">
                 <button type="button" className="button">
@@ -41,8 +59,30 @@ class DigitalTimer extends Component {
                     className="option-img"
                   />
                 </button>
-                <p>Reset</p>
+                <p className="interaction-text">Reset</p>
               </div>
+            </div>
+            <h1 className="set-limit-heading">Set Timer Limit</h1>
+            <div className="timer-limit-section">
+              <button
+                type="button"
+                className="set-button"
+                onClick={this.decreaseTimer}
+                disabled={isTimerRunning}
+              >
+                -
+              </button>
+              <div className="limit-value-container">
+                <p className="limit-value">{timer}</p>
+              </div>
+              <button
+                type="button"
+                className="set-button"
+                onClick={this.increaseTimer}
+                disabled={isTimerRunning}
+              >
+                +
+              </button>
             </div>
           </div>
         </div>
